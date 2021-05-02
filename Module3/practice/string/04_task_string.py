765796b0d88b8ad02af0f5160b06bb3d41551947
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-02 23:33:49 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,29 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+
+# 1-й вариант с передобром символов строки - не обрабатывается последнее слово
+i = 0
+len_word = 0
+num_long_word = 0
+while i < len(text):
+    if text[i] == " " or i == len(text)-1:
+        if len_word > 5:
+            num_long_word += 1
+        len_word = 0
+    else:
+        len_word += 1
+    i += 1
+print(num_long_word)
+
+# 2-ой вариант с методами строки и списка
+
+text1 = text.split()
+i = 0
+c = 0
+while i < len(text1):
+    if len((text1[i])) > 5:
+        c += 1
+    i += 1
+
+print(c)
