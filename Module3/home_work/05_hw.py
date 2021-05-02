5664791f1d65ae377fa122852d0c051bf118d028
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-03 01:15:40 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,16 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+
+# 1-й вариант
+longest_name_index = 0
+name_lnt = 0
+for name in names:
+    if len(name) > name_lnt:
+        name_lnt = len(name)
+        longest_name_index = names.index(name)
+print(names[longest_name_index])
+
+# 2-й вариант
+names.sort(key=len)
+print(names[-1])
