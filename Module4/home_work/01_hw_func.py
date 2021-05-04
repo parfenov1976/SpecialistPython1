2b7276957c3add7ba46b5b6f45dfff5f3c84a36f
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-04 22:00:15 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -2,9 +2,25 @@
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
+def rev(number):
+    rev_n = 0
+    copy_n = number
+    while copy_n:
+        rev_n = 10 * rev_n + copy_n % 10
+        copy_n = copy_n // 10
+    return rev_n
+
+
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    rev_ticket_number = rev(ticket_number)
+    left_half = 0
+    right_half = 0
+    for c in range(len(str(ticket_number)) // 2):
+        left_half = left_half + ticket_number % 10
+        ticket_number = ticket_number // 10
+        right_half = right_half + rev_ticket_number % 10
+        rev_ticket_number = rev_ticket_number // 10
+    return bool(left_half == right_half)
 
 
 # Тестируем функцию
