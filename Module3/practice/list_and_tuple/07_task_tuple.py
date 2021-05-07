208b1dd6dbe031d6048364cfbb7230835e6ab97e
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-07 21:20:57 +0300

    Update 07_task_tuple.py

--- Module3/practice/list_and_tuple/07_task_tuple.py
+++ Module3/practice/list_and_tuple/07_task_tuple.py
@@ -1,5 +1,9 @@
 # Дан кортеж заполненный целыми числами
 # Найдите самый большой элемент кортежа
 tup = (2, 4, 6, -4, 12, 0, 5)
+max_el = 0
 
-# TODO: your code here
+for el in tup:
+    if el > max_el:
+        max_el = el
+print(max_el)
