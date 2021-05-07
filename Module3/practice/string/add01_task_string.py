c5b632ae1ecaee8c4929ff03b65fdee7b498ec2d
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-07 20:53:21 +0300

    Update add01_task_string.py

--- Module3/practice/string/add01_task_string.py
+++ Module3/practice/string/add01_task_string.py
@@ -7,4 +7,17 @@ text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 # Примечание: обратите внимание на перенос длинной строки на новую строку
 
-# TODO: your code here
+symbols_to_replace = ",.!?-"
+
+for c in symbols_to_replace:
+    text = text.replace(c, "")
+cnt = 0  # счетчик слов
+word = ""
+for c in range(len(text)):
+    if text[c] != " ":
+        word = word + text[c]
+        continue
+    if len(word) > 7:
+        cnt += 1
+    word = ""
+print(cnt)
