04ea33e8d8e48296a7d0a2cc2c8ab35dd206a972
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-08 14:18:30 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,24 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+
+f = open("data/limericks.txt", "r", encoding="utf-8")
+# for line in f:
+#     print(line.rstrip())
+#
+# print("=======================")
+
+f.seek(0)
+res = f.readlines()
+line = " ".join(res)
+spacers = [" ", "\n", "\t"]
+for space in spacers:
+    line = line.replace(space, "")
+print(len(line))
+
+f.seek(0)
+count_limericks = 1
+for line in f:
+    if line.strip() == "":
+        count_limericks += 1
+print(count_limericks)
