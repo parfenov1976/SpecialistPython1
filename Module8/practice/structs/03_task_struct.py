366d4a6d6236af98f7f38458cfca9d040da91eb7
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-15 15:08:30 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,32 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+
+import random
+
+random.seed(20)
+
+
+def rnd_numbers_lst(n, at, to):
+    numbers = []
+    for _ in range(n):
+        numbers.append(random.randint(at, to))
+    return numbers
+
+
+# задача 1
+lst = rnd_numbers_lst(10, -50, 50)
+print(lst)
+new_lst = []
+
+for el in lst:
+    if el <= 10:
+        new_lst.append(el)
+print(len(new_lst))
+
+# задача 2
+print(sum(list(filter(lambda n: n > 0, lst))))
+
+# задача 3
+even_list = list(filter(lambda n: n % 2 == 0, lst))
+print(sum(even_list) / len(even_list))
