81dc9c9a42498567b8942f0dd6a8184a73184228
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-15 15:15:23 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,7 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+new_dict = {}
+new_dict.update(*[dictionary_1])
+new_dict.update(*[dictionary_2])
+print(new_dict)
