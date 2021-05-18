f92e71dd8b537359b0d98120ccd5e7fb27bd05c5
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-18 21:58:58 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -1,4 +1,9 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех положительных элементов.
 
-# TODO: your code here
+numbers = [10, -2, -1, 5, 9, 4, -3]
+sum_of_numbers = 0
+for number in numbers:
+    if number > 0:
+        sum_of_numbers += number
+print(sum_of_numbers)
