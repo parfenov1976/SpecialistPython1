581c850221a6c473e9f420fb3f9ad878284e7933
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-18 21:55:56 +0300

    Update 02_task_dict.py

--- Module3/practice/dict/02_task_dict.py
+++ Module3/practice/dict/02_task_dict.py
@@ -7,3 +7,19 @@ values = ['Петр', 'Первый', 42, 1300]
 # TODO: your code here
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
+
+# 1-ый вариант
+# my_dict = {}
+# for i in range(len(keys)):
+#     my_dict[keys[i]] = values[i]
+# print(my_dict)
+
+# 2-й вариант
+# my_dict = {}
+# for key, value in zip(keys, values):
+#     my_dict[key] = value
+# print(my_dict)
+
+# 3-й вариант - предпочтительный
+my_dict = dict(zip(keys, values))
+print(my_dict)
