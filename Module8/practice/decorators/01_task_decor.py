891c36b401db3f3c94e0e6fc48bd8146f34ea3e2
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-18 21:17:56 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,26 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+
+def stars(f):
+    def wrapper(*args):
+        print('*' * (len(str(f(*args))) + 2))
+        print(f'*{f(*args)}*')
+        print('*' * (len(str(f(*args))) + 2))
+    return wrapper
+
+
+@stars
+def sum(a, b):
+    return a + b
+
+sum(5, 12)
+# res = stars(sum)
+# print(res(5, 7))
+
+@stars
+def print_deco(n):
+    return n
+
+print_deco('Привет')
+print_deco(500)
