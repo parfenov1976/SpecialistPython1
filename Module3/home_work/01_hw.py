4b2f804355ccffd33f8992d16eb6c6c75a6ed49e
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-05-18 21:49:42 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -3,7 +3,16 @@
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
-# TODO: your code here
+# 1-й вариант
+string = ', '.join(names)
+print(string)
+
+# 2-й вариант
+string = ""
+for name in names:
+    string += name + ', '
+string = string[:-2]
+print(string)
 
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
