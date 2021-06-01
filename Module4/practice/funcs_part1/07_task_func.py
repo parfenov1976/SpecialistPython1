f750d6c5644278691963e684ddd4bf1c0bfe2ae4
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-01 23:07:19 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,13 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def time(secondss):
+    ss = secondss % 60
+    mm = (secondss // 60) % 60
+    hh = (secondss // 60) // 60
+    return f"{hh:02}:{mm:02}:{ss:02}"
+
+
+seconds = int(input("Введите количество секунд: "))
+print(time(seconds))
