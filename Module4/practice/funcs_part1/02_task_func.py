403f704d7cea8ed8707e1d997fa3ea77dc9fcabe
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-01 23:02:47 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -4,7 +4,13 @@
 # * попробуйте решить данную задачу, не преобразуя число к строке
 
 def palindrome(number):
-    pass
+    # return str(number) == str(number)[::-1]
+    rev_n = 0
+    copy_n = number
+    while copy_n:
+        rev_n = 10 * rev_n + copy_n % 10
+        copy_n = copy_n // 10
+    return number == rev_n
 
 
 # Тестируем функцию
@@ -13,3 +19,6 @@ print(palindrome(3443))
 print(palindrome(1234541))
 print(palindrome(1234321))
 print(palindrome(77777))
+
+# if palindrome(4567654):
+#     print("Число палиндром")
