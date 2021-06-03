c28a301fde8e36dbe655825cf4cb44bafd067531
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-03 22:01:22 +0300

    Add files via upload

---
+++ Module4/practice/funcs_part1/09_task_func.py
@@ -0,0 +1,23 @@
+# Напишите функцию находящую n-ое число Фибоначчи.
+def fibonacci(n):
+    """Compute the Fibonacci number
+
+    :param n: the number of the element in numeric sequence of Fibonacci numbers -> int
+    :return: Fibonacci number -> int
+    """
+    if n == 1:
+        return 0
+    elif n in (1, 2):
+        return 1
+    f = [0, 1]
+    i = 2
+    while i < n:
+        i += 1
+        fs = f[1]
+        f[1] = f[0] + f[1]
+        f[0] = fs
+    return f[1]
+
+
+n = int(input("Введите номе числа Фибоначчи n: "))
+print(fibonacci(n))
