492bf7645ecc48831d2d0f338f4d2a72c63e7f1c
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-06 19:50:01 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,13 +1,11 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
 def fibonacci(n):
-    """Compute the Fibonacci number. Recursion.
+    """Compute the Fibonacci number
 
     :param n: the number of the element in numeric sequence of Fibonacci numbers -> int
     :return: Fibonacci number -> int
     """
-    if n == 1:
-        return 0
-    elif n in (1, 2):
+    if n in (1, 2):
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
