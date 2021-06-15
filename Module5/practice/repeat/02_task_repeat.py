b81bddb066709b9a18f7ee33e4aa440883314ca7
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-15 22:19:36 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -4,4 +4,17 @@
 #  Пример палиндрома: 12321
 
 def palindrome(number):
-    pass
+    return str(number) == str(number)[::-1]
+    # rev_number = 0
+    # copy_number = number
+    # while copy_number:
+    #     rev_number = 10 * rev_number + copy_number % 10
+    #     copy_number = copy_number // 10
+    # return rev_number == number
+
+
+n = int(input("Введите число: "))
+if palindrome(n):
+    print("Число палиндром")
+else:
+    print("Число не палиндром")
