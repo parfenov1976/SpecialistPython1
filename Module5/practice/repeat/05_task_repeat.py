6f4b72467ab1a223078e2bbcb6f4d11b6749f2ed
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-15 22:22:46 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,12 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+
+def pagination(num_items, items_on_page):
+    return num_items % items_on_page or items_on_page
+
+
+print(pagination(245, 5))
+print(pagination(125, 10))
+print(pagination(19, 10))
+print(pagination(117, 20))
