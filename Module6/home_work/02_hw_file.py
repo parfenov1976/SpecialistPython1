c88a09e485bdf3af7ee70d21862f2972e6b9a432
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-29 20:28:45 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -2,4 +2,14 @@
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
 with open("data/info.txt", "r") as f:
-    pass
+    summ = 0
+    non_number = []
+    for line in f:
+        try:
+            summ += int(line.rstrip())
+        except ValueError:
+            non_number.append(line.rstrip())
+    if len(non_number) > 0:
+        print(f"Имеются не числовые значения: {', '.join(non_number)}, данные значения исключены.")
+
+    print(f"Сумма всех чисел равна {summ}")
