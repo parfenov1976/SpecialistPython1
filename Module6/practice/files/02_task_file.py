e94645d07bd70c9c598a8fcca14497f458d5e9b2
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-29 20:52:11 +0300

    Update 02_task_file.py

--- Module6/practice/files/02_task_file.py
+++ Module6/practice/files/02_task_file.py
@@ -4,3 +4,20 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+
+f = open("data/sold.txt", "r", encoding="utf-8")
+
+goods_str = []
+goods_float = []
+for line in f:
+    goods_str += line.split()
+print(goods_str)
+
+for good in goods_str:
+    goods_float.append(float(good))
+
+print(goods_float)
+
+print("sum = ", sum(goods_float))
+print("max = ", max(goods_float))
+print("max = ", min(goods_float))
