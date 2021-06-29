6ee622e9661ec3ba933a62960f31d2f2327a9409
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-06-29 21:14:03 +0300

    Update 03_task_file.py

--- Module6/practice/files/03_task_file.py
+++ Module6/practice/files/03_task_file.py
@@ -6,3 +6,33 @@
 # 2. Какова выручка магазина по каждому типу товаров
 # 3. Какой тип товара был продан на самую большую сумму
 # 4. Сколько различных типов товаров было продано за день
+
+f = open("data/items_sold.txt", "r", encoding="utf-8")
+
+items_dict = {}
+for line in f:
+    items_list = line.split()
+    for item in items_list:
+        pair = item.split(":")
+        if items_dict.get(pair[0]) is not None:
+            items_dict[pair[0]] += float(pair[1])
+        else:
+            items_dict[pair[0]] = float(pair[1])
+
+print(items_dict)
+print(sum(items_dict.values()))
+
+for key, value in items_dict.items():
+    print(f"{key} : {value:.2f}")
+
+max_item = 0
+max_item_type = ""
+for key, value in items_dict.items():
+    if value > max_item:
+        max_item = value
+        max_item_type = key
+print(max_item_type)
+print("Различных товаров продано:", len(items_dict))
+
+
+
