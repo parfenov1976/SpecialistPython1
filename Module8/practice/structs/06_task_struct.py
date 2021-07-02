2efb6e0a69fae85411ceb10d94e10ec2400ba884
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-07-02 16:21:27 +0300

    Update 06_task_struct.py

--- Module8/practice/structs/06_task_struct.py
+++ Module8/practice/structs/06_task_struct.py
@@ -15,9 +15,6 @@ names = ["Алексей", "Иван", "Александр", "Борис", "Бр
 surnames = ["Алексеев", "Андреев", "Брагин", "Бочкин", "Вескин", "Варенин", "Григораш", "Климов", "Якин",
             "Ложкин", "Колпин", "Минский", "Николаев", "Орешкин", "Петров", "Иванов", "Тяпкин"]
 profession = ["инженер", "бетонщик", "сварщик", "такелажник", "охранник", "бухгалтер", "прораб"]
-print(len(names))
-print(len(surnames))
-
 list_size = input('Введите размер списка: ')
 emp_list = []
 for i in range(int(list_size)):
@@ -25,6 +22,7 @@ for i in range(int(list_size)):
         {'Имя': names[randint(1, len(names) - 1)], 'Фамилия': surnames[randint(1, len(surnames) - 1)],
          'Возраст': str(randint(18, 65)), 'Профессия': profession[randint(1, len(profession) - 1)],
          'Зарплата': randint(10000, 50000)})
-
+emp_list = sorted(emp_list, key=lambda y: y.get('Имя'))
+emp_list = sorted(emp_list, key=lambda x: x.get('Фамилия'))
 for el in emp_list:
     print(el)
