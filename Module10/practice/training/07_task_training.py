76a4cd1dfe9444ba470efc088ced0b294e66c463
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-11-01 18:54:26 +0300

    Update 07_task_training.py

--- Module10/practice/training/07_task_training.py
+++ Module10/practice/training/07_task_training.py
@@ -1,1 +1,8 @@
 # Напишите программу генерирующую и записывающую в файле произвольное 20000-значное целое число.
+from random import randint
+
+number = ''
+while len(str(number)) < 20000:
+    number += str(randint(0, 10))
+with open('data.txt', 'w', encoding='UTF-8') as f:
+    f.write(number)
