9012ed9f53752c9dd28baac8b61f038be9e79a88
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-11-01 15:13:31 +0300

    Update 02_task_funcfact.py

--- Module10/practice/funcs/02_task_funcfact.py
+++ Module10/practice/funcs/02_task_funcfact.py
@@ -4,4 +4,27 @@
 # 7! = 1*2*3*4*5*6*7
 
 def factorial(n):
-    pass
+    """
+    Рекурсивная функция
+    :param n: int
+    :return: int
+    """
+    if n > 1:
+        return n * factorial(n - 1)
+    return 1
+
+def fact(n):
+    """
+    Прямая итерация
+    :param n: int
+    :return: int
+    """
+    f = 1
+    while n > 1:
+        f *= n
+        n -= 1
+    return f
+
+n = int(input('Введите чило: '))
+print(f'N!= {factorial(n)}')
+print(f'N!= {fact(n)}')
