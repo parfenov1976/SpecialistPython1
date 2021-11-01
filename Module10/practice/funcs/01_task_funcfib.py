88e9d5d81855031fad6d4d7d7ed3df9f8c505e78
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-11-01 14:54:30 +0300

    Update 01_task_funcfib.py

--- Module10/practice/funcs/01_task_funcfib.py
+++ Module10/practice/funcs/01_task_funcfib.py
@@ -3,4 +3,31 @@
 # (сумма элемента равна сумме двух предыдущих)
 
 def fib(n):
-    pass
+    """
+    Решение в лоб
+    :param n:
+    :return:
+    """
+    a = b = 1
+    i = 2
+    while i < n:
+        i += 1
+        a, b = b, a + b
+    return b
+
+
+def fibonacci(n):
+    """
+    Рекурсивная функция
+    :param n:
+    :return:
+    """
+    if n in (1, 2):
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+n = int(input("Введите номе числа Фибоначчи n: "))
+print(fib(n))
+print(fibonacci(n))
